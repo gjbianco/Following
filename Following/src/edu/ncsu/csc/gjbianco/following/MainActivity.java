4486edef0776d3da5a5c4fde863d737f6bdfafab
Guy J. Bianco IV <gjbianco@ncsu.edu>
2012-07-25 22:14:07 -0400

    button toggles GPS on but not off

--- Following/src/edu/ncsu/csc/gjbianco/following/MainActivity.java
+++ Following/src/edu/ncsu/csc/gjbianco/following/MainActivity.java
@@ -1,54 +1,63 @@
 package edu.ncsu.csc.gjbianco.following;
 
-import com.google.android.maps.MapView;
-
 import android.app.Activity;
 import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
 public class MainActivity extends Activity
 {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-    	// call parent onCreate()
-        super.onCreate(savedInstanceState);
-        
-        // set Activity layout
-        setContentView(R.layout.main);
-        
-        // get textview and button
-        TextView mainTextView = (TextView) findViewById(R.id.textView1);
-        ToggleButton toggle = (ToggleButton) findViewById(R.id.toggleButton1);
-        
-        // acquire reference to system Location Manager
-        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        
-        // define location listener
-        LocationListener locationListener = new LocationListener() {
-        	public void onLocationChanged(Location location) {
-        		// called when a new location is found by network location provider
-		        TextView mainTextView = (TextView) findViewById(R.id.textView1);
-        		mainTextView.setText(location.toString());
-        	}
-        	
-        	public void onStatusChanged(String provider, int status, Bundle extras) {}
-        	
-        	public void onProviderEnabled(String provider) {}
-        	
-        	public void onProviderDisabled(String provider) {}
-        };
-        
-        // register listener with Location Manager
-//        if(toggle.isChecked())
-	        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-//        else
-//        	locationManager.removeUpdates(locationListener);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		// call parent onCreate()
+		super.onCreate(savedInstanceState);
+
+		// set Activity layout
+		setContentView(R.layout.main);
+		
+		ToggleButton toggle = (ToggleButton) findViewById(R.id.toggleButton1);
+
+		OnCheckedChangeListener occl = new OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				// acquire reference to system Location Manager
+				LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+				// define location listener
+				LocationListener locationListener = new LocationListener() {
+					public void onLocationChanged(Location location) {
+						TextView mainTextView = (TextView) findViewById(R.id.textView1);
+
+						int latitude  = (int) location.getLatitude();
+						int longitude = (int) location.getLongitude();
+
+						String out = "lat: " + latitude + "\nlong: " + longitude;
+						mainTextView.setText(out);
+					}
+
+					public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+					public void onProviderEnabled(String provider) {}
+
+					public void onProviderDisabled(String provider) {}
+				};
+				
+				// de/register listener with Location Manager
+				if(isChecked)
+					locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+				else
+					locationManager.removeUpdates(locationListener);
+			}
+		};
+
+		toggle.setOnCheckedChangeListener(occl);
+	}
 }
