a2789c9098b5a9b65d0ea9cafffaf62875435ddd
Guy J. Bianco IV <gjbianco@ncsu.edu>
2012-07-27 14:37:18 -0400

    GPS testing works perfectly

--- Following/src/edu/ncsu/csc/gjbianco/following/MainActivity.java
+++ Following/src/edu/ncsu/csc/gjbianco/following/MainActivity.java
@@ -13,6 +13,10 @@ import android.widget.ToggleButton;
 
 public class MainActivity extends Activity
 {
+	private LocationManager  locationManager;
+	private LocationListener locationListener;
+	private boolean          gpsState;
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState)
@@ -29,35 +33,54 @@ public class MainActivity extends Activity
 			@Override
 			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 				// acquire reference to system Location Manager
-				LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+				locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-				// define location listener
-				LocationListener locationListener = new LocationListener() {
-					public void onLocationChanged(Location location) {
-						TextView mainTextView = (TextView) findViewById(R.id.textView1);
-
-						int latitude  = (int) location.getLatitude();
-						int longitude = (int) location.getLongitude();
-
-						String out = "lat: " + latitude + "\nlong: " + longitude;
-						mainTextView.setText(out);
-					}
-
-					public void onStatusChanged(String provider, int status, Bundle extras) {}
-
-					public void onProviderEnabled(String provider) {}
-
-					public void onProviderDisabled(String provider) {}
-				};
-				
-				// de/register listener with Location Manager
-				if(isChecked)
+				if(isChecked) {
+					// define location listener
+					locationListener = new LocationListener() {
+						public void onLocationChanged(Location location) {
+							TextView mainTextView = (TextView) findViewById(R.id.textView1);
+	
+							int latitude  = (int) location.getLatitude();
+							int longitude = (int) location.getLongitude();
+	
+							String out = "lat: " + latitude + "\nlong: " + longitude;
+							mainTextView.setText(out);
+						}
+	
+						public void onStatusChanged(String provider, int status, Bundle extras) {}
+	
+						public void onProviderEnabled(String provider) {}
+	
+						public void onProviderDisabled(String provider) {}
+					};
+					
+					// register listener with Location Manager
 					locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-				else
+					gpsState = true;
+				}
+				else {
+					// deregister listener with Location Manager
 					locationManager.removeUpdates(locationListener);
+					gpsState = false;
+				}
 			}
 		};
 
 		toggle.setOnCheckedChangeListener(occl);
+	}
+	
+	@Override
+	public void onPause() {
+		super.onPause();
+		if(gpsState)
+			locationManager.removeUpdates(locationListener);
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		if(gpsState)
+			locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
 	}
 }
