b98dfeed4d6dba4f585342565784bfbc0f589b03
Guy J. Bianco IV <gjbianco@ncsu.edu>
2012-07-31 22:23:29 -0400

    added marker.png added FollowingItemizedOverlay

---
+++ Following/src/edu/ncsu/csc/gjbianco/following/FollowingItemizedOverlay.java
@@ -0,0 +1,55 @@
+package edu.ncsu.csc.gjbianco.following;
+
+import java.util.ArrayList;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.OverlayItem;
+
+/**
+ * Used for adding markers to MapView in Following Activity.
+ * <p>Heavily based off of tutorial found <a href=https://developers.google.com/maps/documentation/android/hello-mapview>here</a>.
+ * @author Guy Bianco
+ *
+ */
+class FollowingItemizedOverlay extends ItemizedOverlay {
+	private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
+	private Context mContext;
+
+	public FollowingItemizedOverlay(Following following, Drawable defaultMarker) {
+		super(boundCenterBottom(defaultMarker));
+	}
+	
+	public FollowingItemizedOverlay(Drawable defaultMarker, Context context) {
+		super(boundCenterBottom(defaultMarker));
+		mContext = context;
+	}
+
+	@Override
+	protected boolean onTap(int index) {
+		OverlayItem item = mOverlays.get(index);
+		AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
+		dialog.setTitle(item.getTitle());
+		dialog.setMessage(item.getSnippet());
+		dialog.show();
+		return true;
+	}
+
+	@Override
+	protected OverlayItem createItem(int i) {
+		return mOverlays.get(i);
+	}
+
+	@Override
+	public int size() {
+		return mOverlays.size();
+	}
+	
+	void addOverlay(OverlayItem overlay) {
+		mOverlays.add(overlay);
+		populate();
+	}
+}
